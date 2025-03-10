fef09dc5351fae368ce03aa54a1e010ff672b218
devbalth <devbalth@outlook.com>
2025-03-10 19:25:04 +0800

    update files

---
+++ scripts/account.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form');
+const username = document.querySelector('#email');
+const password = document.querySelector('#password');
+
+const account = new Account(client);
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    const userEmail = username.value;
+    const userPass = password.value;
+
+    form.reset();
+
+    account.create('[USER_ID]', userEmail, userPass).then(() => {
+        window.location.replace('https://devbalth.github.io/imagepost/')
+    })
+
+
+    
+});
--- scripts/setup.js
+++ scripts/setup.js
@@ -1,5 +1,15 @@
-const { Client } = Appwrite;
+// Module
+
+const { Client, Account, ID } = Appwrite;
+
+
+
+// Define
 
 const client = new Client();
+
+
+
+// Config
 
 client.setProject('67cebd6b000aed96da3c');
